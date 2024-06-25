8f6e2154da55ed2eb16f0a02b67d7913b73efab9
Ramon Risério <127441067+ramonriserio@users.noreply.github.com>
2024-06-25 19:03:18 -0300

    New files to swapoff and node info

---
+++ 000-node-info.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Exibe informações das interfaces de rede
+echo "Interfaces de Rede:"
+echo "---------------------"
+
+# Lista todas as interfaces
+for interface in $(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2}' | sed 's/ //g'); do
+    echo "Interface: $interface"
+    echo "  Endereço IP: $(ip addr show dev $interface | grep -oP 'inet \K[\d.]+')"
+    echo "  Endereço MAC: $(ip link show $interface | awk '/ether/ {print $2}')"
+done
+
+# Exibe o product_uuid
+echo ""
+echo "Product UUID:"
+echo "--------------"
+sudo cat /sys/class/dmi/id/product_uuid
